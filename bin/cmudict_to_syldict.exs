3d806c936b99e31c15a410379b40acca39a58db6
Louis Pilfold <louis@lpil.uk>
2015-05-09 19:57:33 +0100

    cmudict to syldict

---
+++ bin/cmudict_to_syldict.exs
@@ -0,0 +1,12 @@
+cmudict = File.stream!( "cmudict" )
+{:ok, syldict} = File.open( "syldict", [:write] )
+
+starts_with_vowel = &String.match?( &1, ~r/\A[AEIOU]/ )
+
+for line <- cmudict do
+  if starts_with_vowel.(line) do
+    [word|syls] = String.split( line )
+    count       = Enum.filter( syls, starts_with_vowel ) |> length
+    IO.write( syldict, "#{ word } #{ count }\n" )
+  end
+end
